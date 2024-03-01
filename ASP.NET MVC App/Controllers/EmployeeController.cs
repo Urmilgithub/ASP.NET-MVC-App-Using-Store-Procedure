f562141bf12e5a88f2aab443e42e8db47f200b8e
Urmilgithub <urmilpatelit27@gmail.com>
2024-03-01 23:05:35 +0530

    feat: Added Insert mehod of employee conroller

--- ASP.NET MVC App/Controllers/EmployeeController.cs
+++ ASP.NET MVC App/Controllers/EmployeeController.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data; // used to store data in table
+using System.Data.SqlClient; // For sql server connection
+using System.Configuration; // Fore web config path
 
 namespace ASP.NET_MVC_App.Controllers
 {
     public class EmployeeController : Controller
     {
+
+        string stringconnection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
         // GET: Employee
         public ActionResult Index()
         {
@@ -18,6 +24,18 @@ namespace ASP.NET_MVC_App.Controllers
         [HttpPost]
         public ActionResult Index(Employee employee)
         {
+            SqlConnection connect = new SqlConnection(stringconnection);
+            if(connect.State == ConnectionState.Closed)
+            {
+                connect.Open();
+            }
+            string str = "Insert into Employee_tbl(Name,Email,Gender,Contact,Password,Address,TC) values ('" + employee.Name +"', '" + employee.Email +"','" + employee.Gender +"','" + Convert.ToInt32(employee.Contact) + "','" + employee.Password +"', '" + employee.Address +"', '" + employee.tc +"')";
+            SqlCommand cmd = new SqlCommand(str, connect);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            ModelState.Clear();
+            TempData["msg"] = "Successfully Added Data";
+            TempData.Keep();
             return View();
         }
     }
