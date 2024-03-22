dc77bb4579a7fdb7176244e599c287df0cef7da6
Urmilgithub <urmilpatelit27@gmail.com>
2024-03-23 00:11:11 +0530

    feat: created strongly type form

--- ASP.NET MVC App/Controllers/LoginController.cs
+++ ASP.NET MVC App/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP.NET_MVC_App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,12 @@ namespace ASP.NET_MVC_App.Controllers
     {
         // GET: Login
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee employee)
         {
             return View();
         }
