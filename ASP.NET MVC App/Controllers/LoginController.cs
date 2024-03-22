aa23ac8d985474786d572dc0cd00dc413f8226bc
Urmilgithub <urmilpatelit27@gmail.com>
2024-03-22 23:31:57 +0530

    feat: Add login form

---
+++ ASP.NET MVC App/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
